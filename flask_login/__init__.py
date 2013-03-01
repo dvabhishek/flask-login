1a17438014d23aedb029eabc2c3c3a83310a440f
Max Countryman <max.countryman@gmail.com>
2013-02-28 16:24:16 -0800

    cleanup

---
+++ flask_login/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+'''
+flask.ext.login
+===============
+
+    This module provides user session management for Flask. It lets you log
+    your users in and out in a database-independent manner.
+
+    :copyright: (c) 2011 by Matthew Frazier.
+    :license:   MIT/X11, see LICENSE for more details.
+'''
+
+__version_info__ = (0, 1, 2)
+__version__ = '.'.join(__version_info__)
+__author__ = 'Matthew Frazier'
+__license__ = 'MIT/X11'
+__copyright__ = '(c) 2011 by Matthew Frazier'
+__all__ = ['LoginManager']
+
+from flask_login import
